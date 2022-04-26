1f8a7861561ec97d9ef2a0d5823326bcd08a1a28
Marcos Vinicius Santos Silva <vilok13@outlook.com>
2022-04-25 22:24:00 -0300

    Atividade difícil

---
+++ EXERCÍCIO1EMGRUPO.c
@@ -0,0 +1,37 @@
+/*Faça um programa que ordene, em ordem alfabética crescente, o vetor Nomes que contém os seguchares dados: Miriam, Sueli, Ana, Miguel, Daniel, Tales, Humberto. Utilize o método de ordenação por trocas. */
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+char nome [7][9] = {"Miriam","Sueli","Ana","Miguel","Daniel","Tales","Humberto"};
+
+void ORDENA (){
+    int x,y,r;
+    char aux[9];
+    for(x=0; x<=6; x++){
+        for(y = x+1; y<=6; y ++){
+            r = strcmp(nome[x], nome [y]);
+            if(r > 0){
+                strcpy(aux, nome[x]);
+                strcpy(nome[x],nome[y]);
+                strcpy(nome[y], aux);
+            }
+        }
+    }
+}
+main()
+{
+    int x;
+  
+    
+
+    ORDENA();
+
+    printf("Nomes ordenados: \n");
+    for(x = 0; x <= 9; x++){
+        puts(nome [x]);
+    }
+    
+}
